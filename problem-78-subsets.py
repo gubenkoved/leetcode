536b1cc3a8b89b0c6e5b37a7a1afaacec62fd0d3
Eugene Gubenkov <gubenkoved@gmail.com>
2022-02-14 13:12:14 +0400

    added problem-78-subsets.py - version 2

--- problem-78-subsets.py
+++ problem-78-subsets.py
@@ -16,8 +16,60 @@ def subsets(a, k=None):
                     yield frozenset(list(combination) + [el])
 
 
-# TODO: solution that tracks "available" elements on each recursive call to solve
-#  uniqueness issue more efficiently
+def combinations_no_repetitions(items, k):
+    n = len(items)
+    result = []
+    cur = []
+
+    def gen(last_idx):
+        l = k - len(cur)
+        for idx in range(last_idx+1, n-l+1):
+            cur.append(items[idx])
+
+            if len(cur) == k:
+                result.append(list(cur))
+            else:
+                gen(idx)
+
+            cur.pop(-1)
+
+    gen(-1)
+    return result
 
 
-print([list(x) for x in set(subsets([1, 2, 3]))])
+def combinations_no_repetitions_v2(items, k):
+    n = len(items)
+    result = []
+    cur = [None] * k
+    cur_ptr = 0
+
+    def gen(last_idx):
+        nonlocal cur_ptr
+        l = k - cur_ptr
+        for idx in range(last_idx+1, n-l+1):
+            cur[cur_ptr] = items[idx]
+            cur_ptr += 1
+
+            if cur_ptr == k:
+                result.append(list(cur))
+            else:
+                gen(idx)
+
+            cur_ptr -= 1
+
+    gen(-1)
+    return result
+
+
+def powerset(items):
+    result = [[]]
+    for k in range(1, len(items) + 1):
+        # result.extend(combinations_no_repetitions(items, k))
+        result.extend(combinations_no_repetitions_v2(items, k))
+    return result
+
+
+# print([list(x) for x in set(subsets([1, 2, 3]))])
+# print(combinations_no_repetitions([1, 2, 3, 4], k=2))
+# print(powerset([1, 2, 3]))
+print(powerset([1, 2, 3, 4]))
