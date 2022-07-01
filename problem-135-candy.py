9a473092182bcbc72a2ad5cdf17c84e7e91533df
Eugene Gubenkov <gubenkoved@gmail.com>
2022-07-02 01:43:06 +0300

    problem-135-candy.py PASS

--- problem-135-candy.py
+++ problem-135-candy.py
@@ -3,7 +3,7 @@ from typing import List
 
 # TLE on 44/48
 class Solution:
-    def candy(self, ratings: List[int]) -> int:
+    def candy_v1(self, ratings: List[int]) -> int:
         n = len(ratings)
         result = [1] * n
         changed = True
@@ -18,8 +18,27 @@ class Solution:
                     changed = True
         return sum(result)
 
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        result = [1] * n
+
+        # forward pass
+        for idx in range(1, n):
+            if ratings[idx] > ratings[idx - 1] and result[idx] <= result[idx - 1]:
+                result[idx] = result[idx - 1] + 1
+
+        # backward pass
+        for idx in range(n - 2, -1, -1):
+            if ratings[idx] > ratings[idx + 1] and result[idx] <= result[idx + 1]:
+                result[idx] = result[idx + 1] + 1
+
+        return sum(result)
+
 
 if __name__ == '__main__':
     x = Solution()
     print(x.candy([1, 2, 2]))
     print(x.candy([1, 0, 2]))
+    f = open('input.txt')
+    q = eval(f.read())
+    print(x.candy(q))
