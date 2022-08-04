0ae24de1f0801e19d063902b9dfe8818b8b24a1a
Eugene Gubenkov <gubenkoved@gmail.com>
2022-08-04 21:50:16 +0200

    problem-691-stickers-to-spell-word.py 98/101

--- problem-691-stickers-to-spell-word.py
+++ problem-691-stickers-to-spell-word.py
@@ -11,6 +11,19 @@ def overlap_count(x, y):
     return total_count
 
 
+def is_dominated(dominator, dominated):
+    # returns True if dominator has all the chars that dominated has
+    # with either same or bigger amount
+    dominator_counts = Counter(dominator)
+    dominated_counts = Counter(dominated)
+
+    for key in dominated_counts:
+        if dominator_counts.get(key, 0) < dominated_counts[key]:
+            return False
+
+    return True
+
+
 class Solution:
     def minStickers(self, stickers: List[str], target: str) -> int:
         # sanitize the input -- remove all the stickers letters which are not in
@@ -19,9 +32,22 @@ class Solution:
             return ''.join([c for c in sticker if c in target])
 
         stickers = [sanitize(sticker, target) for sticker in stickers]
-        stickers = [x for x in stickers if x]
+        stickers = [''.join(sorted(x)) for x in stickers if x]
+        stickers = list(set(stickers))
         # stickers = sorted(stickers, key=lambda x: overlap_count(x, target),
         #                   reverse=True)
+
+        # remove stickers which are "dominated" by another ones
+        to_remove = set()
+        for sticker_a in stickers:
+            for sticker_b in stickers:
+                if sticker_a != sticker_b and is_dominated(sticker_a, sticker_b):
+                    print('%s dominated by %s' % (sticker_b, sticker_a))
+                    to_remove.add(sticker_b)
+
+        print('dominated stickers: %s' % to_remove)
+        stickers = [x for x in stickers if x not in to_remove]
+        print('stickers left: %s' % stickers)
 
         # see if it is even possible to solve
         if set(target) - set(''.join(stickers)):
@@ -63,4 +89,6 @@ if __name__ == '__main__':
     x = Solution()
     # print(x.minStickers(["with", "example", "science"], target="thehat"))
     # print(x.minStickers(["these","guess","about","garden","him"], target="atomher"))
-    print(x.minStickers(["seven","old","stream","century","energy","period","an","proper","together","sight","carry","milk","appear","winter","field","rather","caught","danger","lake","shall","machine","few","other","test","got","wing","map","finish","though","observe","log","they","foot","path","eat","glad","must","bar","did","of","clear","work","rule","quotient","produce","clean","wild","grass","example","left"], "weresurprise"))
+    # print(x.minStickers(["seven","old","stream","century","energy","period","an","proper","together","sight","carry","milk","appear","winter","field","rather","caught","danger","lake","shall","machine","few","other","test","got","wing","map","finish","though","observe","log","they","foot","path","eat","glad","must","bar","did","of","clear","work","rule","quotient","produce","clean","wild","grass","example","left"], "weresurprise"))
+    # print(x.minStickers(["final","figure","danger","fish","some","product","son","seed","crease","rail","even","death","end","sit","live","behind","start","enough","much","between","test","is","happy","we","north","complete","month","reach","excite","stay","job","fell","letter","noun","seat","exact","than","ago","protect","kept","this","plain","flow","face","bird","sand","rock","roll","root","fact"], "lakeblue"))
+    print(x.minStickers(["indicate","why","finger","star","unit","board","sister","danger","deal","bit","phrase","caught","if","other","water","huge","general","read","gold","shall","master","men","lay","party","grow","view","if","pull","together","head","thank","street","natural","pull","raise","cost","spoke","race","new","race","liquid","me","please","clear","could","reply","often","huge","old","nor"], "fhhfiyfdcwbycma"))
