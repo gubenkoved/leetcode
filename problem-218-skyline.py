1f7c94dff39ae1fdd7eb630d80f497ccc873beaa
Eugene Gubenkov <gubenkoved@gmail.com>
2023-02-12 11:30:36 +0100

    problem-218-skyline.py -- added even faster solution w/o separate tree to get max

--- problem-218-skyline.py
+++ problem-218-skyline.py
@@ -5,7 +5,7 @@ import heapq
 
 class Solution:
     # building is (left, right, height)
-    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+    def getSkyline_v1(self, buildings: List[List[int]]) -> List[List[int]]:
         skyline = []  # list of [left, height]
         events = []
 
@@ -58,10 +58,48 @@ class Solution:
 
         return skyline
 
+    def getSkyline(self, buildings: List[List[int]]) -> List[List[int]]:
+        events = []
+
+        for idx, (left, right, height) in enumerate(buildings):
+            events.append((left, idx, 0))  # open events
+            events.append((right, idx, 1))  # close events
+
+        events.sort()
+
+        skyline = []
+        heap = []
+        prev = 0
+
+        events_cursor = 0
+        while events_cursor < len(events):
+            process_at = events[events_cursor][0]
+
+            while events_cursor < len(events) and events[events_cursor][0] == process_at:
+                at, idx, is_close = events[events_cursor]
+                _, right, h = buildings[idx]
+                if not is_close:
+                    heapq.heappush(heap, (-h, right))
+                events_cursor += 1
+
+            while heap and heap[0][1] <= process_at:
+                heapq.heappop(heap)
+
+            cur_val = -heap[0][0] if heap else 0
+            if prev != cur_val:
+                skyline.append([process_at, cur_val])
+                prev = cur_val
+
+        return skyline
+
 
 if __name__ == '__main__':
     x = Solution()
     print(x.getSkyline([[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]))
     print(x.getSkyline([[0,2,3],[2,5,3]]))
     print(x.getSkyline([[0, 1, 3]]))
+
+    # [[2, 10], [3, 15], [7, 12], [12, 0], [15, 10], [20, 8], [24, 0]]
+    # [[0, 3], [5, 0]]
+    # [[0, 3], [1, 0]]
 
