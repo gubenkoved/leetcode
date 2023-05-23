122ddbe53afea0a72eaae5bf33aa646dcf9e9afc
Eugene Gubenkov <gubenkoved@gmail.com>
2023-05-24 00:22:25 +0200

    problem-879-profitable-schemes.py -- OK, but slow

--- problem-879-profitable-schemes.py
+++ problem-879-profitable-schemes.py
@@ -3,7 +3,7 @@ from typing import List
 
 
 class Solution:
-    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+    def profitableSchemes_bf(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
         count = 0
         k = len(group)
 
@@ -32,11 +32,44 @@ class Solution:
 
         return result
 
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+
+        cache = {}
+
+        def dp(len, n_, p_):
+            cache_key = (len, n_, p_)
+
+            if cache_key in cache:
+                return cache[cache_key]
+
+            if len == 0 or n_ == 0:
+                result = 1 if p_ <= 0 else 0
+                cache[cache_key] = result
+                return result
+
+            # do not commit
+            result = dp(len - 1, n_, p_)
+
+            # commit
+            if n_ >= group[len - 1]:
+                result += dp(len - 1, n_ - group[len - 1], max(0, p_ - profit[len - 1]))
+
+            cache[cache_key] = result
+
+            return result
+
+        return dp(len(group), n, minProfit) % (10 ** 9 + 7)
+
 
 if __name__ == '__main__':
     x = Solution()
     assert x.profitableSchemes(n=5, minProfit=3, group=[2, 2], profit=[2, 3]) == 2
     assert x.profitableSchemes(n=10, minProfit=5, group=[2, 3, 5], profit=[6, 7, 8]) == 7
+    assert x.profitableSchemes(1, 0, [1], [1]) == 2
+    assert x.profitableSchemes(2, 0, [1] * 2, [1] * 2) == 4
+    assert x.profitableSchemes(3, 0, [1] * 3, [1] * 3) == 8
+    assert x.profitableSchemes(10, 1, [1] * 10, [1] * 10) == 1024 - 1
+    assert x.profitableSchemes(10, 0, [1] * 10, [1] * 10) == 1024
     assert x.profitableSchemes(95, 53,
                                [82, 7, 18, 34, 1, 3, 83, 56, 50, 34, 39, 38, 76, 92, 71, 2, 6, 74, 1, 82, 22, 73, 88,
                                 98, 6, 71, 6, 26, 100, 75, 57, 88, 43, 16, 22, 89, 7, 9, 78, 97, 22, 87, 34, 81, 74, 56,
@@ -47,3 +80,7 @@ if __name__ == '__main__':
                                 10, 49, 8, 36, 9, 52, 81, 26, 5, 2, 30, 39, 89, 62, 39, 100, 67, 33, 86, 22, 49, 15, 94,
                                 59, 47, 41, 45, 17, 99, 87, 77, 48, 22, 77, 82, 85, 97, 66, 3, 38, 49, 60,
                                 66]) == 9883351
+    assert x.profitableSchemes(64, 0, [80, 40], [88, 88]) == 2
+    assert x.profitableSchemes(1, 1, [1, 1, 1, 1, 2, 2, 1, 2, 1, 1], [0, 1, 0, 0, 1, 1, 1, 0, 2, 2]) == 4
+    assert x.profitableSchemes(100, 0, [1] * 100, [1] * 100) == 976371285
+    assert x.profitableSchemes(100, 100, [11] * 100, [11] * 100) == 0
