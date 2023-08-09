88165d0d7318f9cc6a2892277a96a2a86f438587
Eugene Gubenkov <gubenkoved@gmail.com>
2023-08-09 21:26:57 +0200

    problem-347-top-k-frequent-elements.py - added O(n) solution

--- problem-347-top-k-frequent-elements.py
+++ problem-347-top-k-frequent-elements.py
@@ -2,6 +2,7 @@ from typing import List
 
 
 class Solution:
+    # O(nlogn)
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq = {}
 
@@ -14,3 +15,27 @@ class Solution:
         freq_items.sort(reverse=True)
 
         return [x for _, x in freq_items[:k]]
+
+    # looked up O(n) version
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freq = {}
+
+        for num in nums:
+            if num not in freq:
+                freq[num] = 0
+            freq[num] += 1
+
+        buckets = [[] for _ in nums]
+
+        for key, freq in freq.items():
+            buckets[-freq].append(key)
+
+        result = []
+
+        for bucket in buckets:
+            if bucket:
+                result.extend(bucket)
+            if len(result) >= k:
+                break
+
+        return result[:k]
