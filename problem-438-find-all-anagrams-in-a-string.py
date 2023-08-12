9aba2a6946b515b98f6c3f3314bf1f88d572b499
Eugene Gubenkov <gubenkoved@gmail.com>
2023-08-12 10:13:32 +0200

    problem-438-find-all-anagrams-in-a-string.py -- pass (very slow)

---
+++ problem-438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        def rolling_hash(x):
+            result = 0
+            for c in x:
+                result ^= ord(c)
+            return result
+
+        def normalize(x):
+            return ''.join(sorted(x))
+
+        def equal_at(start_idx):
+            return normalize(s[start_idx:start_idx+len(p)]) == p
+
+        p = normalize(p)
+        window_hash = rolling_hash(s[:len(p)])
+        target_hash = rolling_hash(p)
+        result = []
+
+        for idx in range(len(s) - len(p) + 1):
+            if idx != 0:
+                window_hash ^= ord(s[idx - 1])
+                window_hash ^= ord(s[idx + len(p) - 1])
+
+            if window_hash == target_hash:
+                if equal_at(idx):
+                    result.append(idx)
+
+        return result
+
+
+if __name__ == '__main__':
+    x = Solution()
+    # print(x.findAnagrams("cbaebabacd", p="abc"))
+    print(x.findAnagrams("abab", p="ab"))
