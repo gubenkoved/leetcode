c2a90ac1424b377d0af2abb2d851675b0777ba63
Eugene Gubenkov <gubenkoved@gmail.com>
2023-08-13 20:17:45 +0200

    problem-1143-longest-common-subsequence.py - faster

--- problem-1143-longest-common-subsequence.py
+++ problem-1143-longest-common-subsequence.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 import sys
 
 
@@ -7,19 +6,30 @@ sys.setrecursionlimit(10000)
 
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        @lru_cache(maxsize=None)
+        cache = {}
+
         def f(i, j):
+            key = i * 2000 + j
+
+            if key in cache:
+                return cache[key]
+
             if text1[i] == text2[j]:
                 if i >= 1 and j >= 1:
-                    return f(i - 1, j - 1) + 1
-                return 1
+                    result = f(i - 1, j - 1) + 1
+                else:
+                    result = 1
             else:
                 best = 0
                 if i - 1 >= 0:
                     best = max(best, f(i - 1, j))
                 if j - 1 >= 0:
                     best = max(best, f(i, j - 1))
-                return best
+                result = best
+
+            cache[key] = result
+
+            return result
 
         return f(len(text1) - 1, len(text2) - 1)
 
