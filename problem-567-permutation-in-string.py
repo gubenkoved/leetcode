f63d4e5f9aece848d9e10bff5bc66f2520fff039
Eugene Gubenkov <gubenkoved@gmail.com>
2023-08-14 23:31:40 +0200

    problem-567-permutation-in-string.py - pass

---
+++ problem-567-permutation-in-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        def rolling_hash(s):
+            result = 0
+            for c in s:
+                result ^= ord(c)
+            return result
+
+        s1 = ''.join(sorted(s1))
+        s1_hash = rolling_hash(s1)
+
+        window_hash = rolling_hash(s2[:len(s1) - 1])
+
+        k = len(s1)
+        for idx in range(len(s2) - k + 1):
+            # hash in new character
+            window_hash ^= ord(s2[idx + k - 1])
+
+            # hash out out-of-window character
+            if idx > 0:
+                window_hash ^= ord(s2[idx - 1])
+
+            if window_hash == s1_hash:
+                if s1 == ''.join(sorted(s2[idx:idx+len(s1)])):
+                    return True
+
+        return False
+
+
+if __name__ == '__main__':
+    x = Solution()
+    print(x.checkInclusion(s1="ab", s2="aabb"))
+    print(x.checkInclusion(s1="ab", s2="eidbaooo"))
+    print(x.checkInclusion(s1="ab", s2="eidboaoo"))
