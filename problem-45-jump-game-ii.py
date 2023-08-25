72cfd91de027211a9bff7a8a2dba807a45e37389
Eugene Gubenkov <gubenkoved@gmail.com>
2023-08-26 00:36:45 +0200

    problem-45-jump-game-ii.py - faster solution
    consulted with existing submissions, it is basically the same BFS but a faster versions which implements this edge-case

--- problem-45-jump-game-ii.py
+++ problem-45-jump-game-ii.py
@@ -26,7 +26,24 @@ class Solution:
 
         return -1
 
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        jumps = 0
+        rolling_farthest = 0
+        cur_jump_farthest = 0
+
+        for idx in range(n):
+            rolling_farthest = max(rolling_farthest, idx + nums[idx])
+
+            if idx == cur_jump_farthest and idx != n - 1:
+                print('with %d jumps could reach up to %d' % (jumps, cur_jump_farthest))
+                jumps += 1
+                cur_jump_farthest = rolling_farthest
+
+        return jumps
+
 
 if __name__ == '__main__':
     x = Solution()
-    print(x.jump([2,3,1,1,4]))
+    print(x.jump([2, 3, 1, 1, 4]))
+    print(x.jump([0]))
