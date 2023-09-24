7aac205753e555a128cf5083fddbe5402d837faa
Eugene Gubenkov <gubenkoved@gmail.com>
2023-09-24 20:00:20 +0200

    problem-518-coin-change-2.py - pass, faster
    looked up a faster approach

--- problem-518-coin-change-2.py
+++ problem-518-coin-change-2.py
@@ -24,6 +24,30 @@ class Solution:
 
         return count(amount, coins[0])
 
+    def change(self, amount: int, coins: List[int]) -> int:
+
+        # returns count of ways to get exactly "target" amount using coins up to
+        # the one with index "coin_idx"
+        @lru_cache(maxsize=None)
+        def count(target, coin_idx):
+            if target == 0:
+                return 1
+
+            result = 0
+
+            # suppose we do not use coin at given idx
+            if coin_idx > 0:
+                # same target required but can not use coin at idx "coin_idx"
+                result += count(target, coin_idx - 1)
+
+            # use the coin if we can
+            if coins[coin_idx] <= target:
+                result += count(target - coins[coin_idx], coin_idx)
+
+            return result
+
+        return count(amount, len(coins) - 1)
+
 
 if __name__ == '__main__':
     x = Solution()
