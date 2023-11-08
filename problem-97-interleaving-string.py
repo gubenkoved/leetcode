a344df940b50a8a6100be6570a49bf11ffbaed41
Eugene Gubenkov <gubenkoved@gmail.com>
2023-11-08 22:15:53 +0100

    problem-97-interleaving-string.py - little better

--- problem-97-interleaving-string.py
+++ problem-97-interleaving-string.py
@@ -3,31 +3,32 @@ import functools
 
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n1, n2, n3 = len(s1), len(s2), len(s3)
+
+        if n1 + n2 != n3:
+            return False
 
         @functools.lru_cache(maxsize=None)
-        def check(s1, s2, s3):
-            if len(s1) + len(s2) != len(s3):
-                return False
+        def check(idx1, idx2, idx3):
+            if idx1 == n1:
+                return s2[idx2:] == s3[idx3:]
 
-            if not s1:
-                return s2 == s3
+            if idx2 == n2:
+                return s1[idx1:] == s3[idx3:]
 
-            if not s2:
-                return s1 == s3
+            wanted = s3[idx3]
 
-            wanted = s3[0]
-
-            if s1 and s1[0] == wanted:
-                if check(s1[1:], s2, s3[1:]):
+            if idx1 != n1 and s1[idx1] == wanted:
+                if check(idx1 + 1, idx2, idx3 + 1):
                     return True
 
-            if s2 and s2[0] == wanted:
-                if check(s1, s2[1:], s3[1:]):
+            if idx2 != n2 and s2[idx2] == wanted:
+                if check(idx1, idx2 + 1, idx3 + 1):
                     return True
 
             return False
 
-        return check(s1, s2, s3)
+        return check(0, 0, 0)
 
 
 if __name__ == '__main__':
