d09c6245fd1cf4d6c532959abca978aa6f7fb37d
Eugene Gubenkov <gubenkoved@gmail.com>
2023-11-09 20:50:03 +0100

    problem-221-maximal-square.py - PASS
    Looked up another DP approach which is better

--- problem-221-maximal-square.py
+++ problem-221-maximal-square.py
@@ -5,53 +5,29 @@ class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
         n, m = len(matrix), len(matrix[0])
 
-        # partial sums
-        p = [[0] * m for _ in range(n)]
+        # dp[i][j] is maximal size of square that ends at (i, j) that is
+        # fully filled with "1"
+        dp = [[0] * m for _ in range(n)]
 
-        for i in range(n):
-            for j in range(m):
-                val = int(matrix[i][j])
+        best = 0
+        for row in range(n):
+            for col in range(m):
 
-                if i != 0:
-                    val += int(p[i - 1][j])
+                if matrix[row][col] == "0":
+                    continue
 
-                if j != 0:
-                    val += int(p[i][j - 1])
+                if row == 0 or col == 0:
+                    dp[row][col] = int(matrix[row][col])
+                else:
+                    dp[row][col] = min(
+                        int(dp[row - 1][col]),
+                        int(dp[row][col - 1]),
+                        int(dp[row - 1][col - 1])
+                    ) + 1
 
-                if i != 0 and j != 0:
-                    val -= int(p[i - 1][j - 1])
+                best = max(best, int(dp[row][col]))
 
-                p[i][j] = val
-
-        best_size = 0
-
-        def pf(i, j):
-            if i < 0 or j < 0:
-                return 0
-            return p[i][j]
-
-        # now calculate the answer
-        for size in range(1, min(n, m) + 1):
-            go_to_next_size = False
-
-            for i in range(n - size + 1):
-                if go_to_next_size:
-                    break
-
-                for j in range(m - size + 1):
-                    actual_sum = (
-                            p[i + size - 1][j + size - 1]
-                            - pf(i + size - 1, j - 1)
-                            - pf(i - 1, j + size - 1)
-                            + pf(i - 1, j - 1)
-                    )
-
-                    if actual_sum == size ** 2:
-                        best_size = size
-                        go_to_next_size = True
-                        break
-
-        return best_size ** 2
+        return best ** 2
 
 
 if __name__ == '__main__':
