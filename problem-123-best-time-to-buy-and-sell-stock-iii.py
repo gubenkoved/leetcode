f7317ac39c3cc94dca0df4eeea1ddf3bf2af1583
Eugene Gubenkov <gubenkoved@gmail.com>
2023-11-11 22:19:09 +0100

    problem-123-best-time-to-buy-and-sell-stock-iii.py - PASS

--- problem-123-best-time-to-buy-and-sell-stock-iii.py
+++ problem-123-best-time-to-buy-and-sell-stock-iii.py
@@ -5,49 +5,32 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
 
-        cache = {}
+        # a[i] is best profit using single trx from day zero till day "i"
+        # b[i] is best profit using single trx from day "i" till day "n"
+        # both can be computed in linear time, and then we can get final answer
+        # in single pass as well, so overall will be linear
+        a = [0] * n
+        b = [0] * (n + 1)
 
-        def max_profit(to_day, trx_count) -> int:
-            cache_key = (to_day, trx_count)
-            if cache_key in cache:
-                return cache[cache_key]
+        rolling_min = prices[0]
+        best_profit = 0
+        for day in range(1, n):
+            best_profit = max(best_profit, prices[day] - rolling_min)
+            rolling_min = min(rolling_min, prices[day])
+            a[day] = best_profit
 
-            if to_day < 1:
-                return 0
+        rolling_max = prices[-1]
+        best_profit = 0
+        for day in range(n - 2, -1, -1):
+            best_profit = max(best_profit, rolling_max - prices[day])
+            rolling_max = max(rolling_max, prices[day])
+            b[day] = best_profit
 
-            if trx_count == 1:
-                min_price_so_far = prices[0]
-                result = 0
-                for day in range(to_day + 1):
-                    potential_profit = prices[day] - min_price_so_far
-                    result = max(result, potential_profit)
-                    min_price_so_far = min(min_price_so_far, prices[day])
-                cache[cache_key] = result
-                return result
+        final = 0
+        for day in range(1, n):
+            final = max(final, a[day] + b[day + 1])
 
-            # recursive dive!
-            result = 0
-            for middle_day in range(to_day - 1):
-                cur_result = max_profit(to_day=middle_day, trx_count=trx_count - 1)
-
-                # now we have time from "day" for the last transaction
-                min_price_so_far = prices[middle_day + 1]
-                best_final_sell = 0
-                for final_sell_day in range(middle_day + 2, to_day + 1):
-                    potential_profit = prices[final_sell_day] - min_price_so_far
-                    best_final_sell = max(best_final_sell, potential_profit)
-                    min_price_so_far = min(min_price_so_far, prices[final_sell_day])
-
-                cur_result += best_final_sell
-                result = max(result, cur_result)
-
-            cache[cache_key] = result
-            return result
-
-        return max(
-            max_profit(to_day=n - 1, trx_count=1),
-            max_profit(to_day=n - 1, trx_count=2),
-        )
+        return final
 
 
 if __name__ == '__main__':
