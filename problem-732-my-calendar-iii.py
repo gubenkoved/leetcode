8f48957b9ecf2111323eef61c12e805b73a1b824
Eugene Gubenkov <gubenkoved@gmail.com>
2024-05-27 21:40:50 +0200

    problem-732-my-calendar-iii.py - added naive greedy solution (WA)

---
+++ problem-732-my-calendar-iii.py
@@ -0,0 +1,70 @@
+from typing import List, Tuple
+
+
+class MyCalendarThree:
+
+    def __init__(self):
+        self.layers: List[List[Tuple[int, int]]] = [[]]
+
+    def book(self, startTime: int, endTime: int) -> int:
+        is_handled = False
+        for layer_idx in range(len(self.layers)):
+            layer = self.layers[layer_idx]
+
+            candidate_idx = 0
+            while candidate_idx != len(layer) and layer[candidate_idx][0] < startTime:
+                candidate_idx += 1
+
+            # now check that in this layer it does not intersect with neither left
+            # nor right interval
+            fits = True
+            if candidate_idx > 0:
+                if startTime < layer[candidate_idx - 1][1]:
+                    fits = False
+            if candidate_idx != len(layer):
+                if endTime > layer[candidate_idx][0]:
+                    fits = False
+
+            if fits:
+                layer.insert(candidate_idx, (startTime, endTime))
+                # print('adding (%s, %s) to layer %s' % (startTime, endTime, layer_idx))
+                is_handled = True
+                break
+
+        if not is_handled:
+            # adding a new layer
+            # print('adding new layer for (%s, %s)' % (startTime, endTime))
+            self.layers.append([(startTime, endTime)])
+
+        return len(self.layers)
+
+
+if __name__ == '__main__':
+    def check(bookings, expected):
+        print('checking for %s bookings...' % bookings)
+        x = MyCalendarThree()
+        results = []
+        for booking in bookings:
+            results.append(x.book(booking[0], booking[1]))
+            # if results != expected[:len(results)]:
+            #     assert False, 'Failed at %s, expected %s, got %s' % (booking, expected[len(results) - 1], results[-1])
+        assert expected == results, '%s != %s (expected, actual)' % (expected, results)
+
+    check(
+        [[10, 20], [50, 60], [10, 40], [5, 15], [5, 10], [25, 55]],
+        [1, 1, 2, 3, 3, 3],
+    )
+    check(
+        [[47, 50], [1, 10], [27, 36], [40, 47], [20, 27], [15, 23], [10, 18]],
+        [1, 1, 1, 1, 1, 2, 2],
+    )
+    check(
+        [[47, 50], [1, 10], [27, 36], [40, 47], [20, 27], [15, 23], [10, 18], [27, 36], [17, 25]],
+        [1, 1, 1, 1, 1, 2, 2, 2, 3]
+    )
+    check(
+        [[47, 50], [1, 10], [27, 36], [40, 47], [20, 27], [15, 23], [10, 18], [27, 36], [17, 25], [8, 17], [24, 33],
+         [23, 28], [21, 27], [47, 50], [14, 21], [26, 32], [16, 21], [2, 7], [24, 33], [6, 13], [44, 50], [33, 39],
+         [30, 36], [6, 15], [21, 27], [49, 50], [38, 45], [4, 12], [46, 50], [13, 21]],
+        [1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7]
+    )
