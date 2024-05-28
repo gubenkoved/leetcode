29b0b442a4cb788297ac4342b1f818f11c7da568
Eugene Gubenkov <gubenkoved@gmail.com>
2024-05-28 21:37:51 +0200

    problem-699-falling-squares.py - pass (naive and slow though)
    beats 12.5% by time

--- problem-699-falling-squares.py
+++ problem-699-falling-squares.py
@@ -5,23 +5,36 @@ class Solution:
     # the below probably only works if we add blocks in a way that
     # x coordinate is not decreasing, which is not the case
     def fallingSquares(self, positions: List[List[int]]) -> List[int]:
-        # tracks (end_x, height) in LIFO order
-        stack = []
         results = []
-        cur_tallest = 0
 
-        for start_x, length in positions:
-            end_x = start_x + length
-            while stack and stack[-1][0] <= start_x:
-                stack.pop(-1)
+        # track blocks calculated vertical offset
+        offsets = []
 
-            if stack:
-                stack.append((end_x, stack[-1][1] + length))
-            else:
-                stack.append((end_x, length))
+        def intersects(range_1, range_2):
+            if range_1[0] > range_2[0]:
+                range_1, range_2 = range_2, range_1
+            return range_2[0] < range_1[1]
 
-            cur_tallest = max(cur_tallest, stack[-1][1])
-            results.append(cur_tallest)
+        rolling_max = 0
+
+        # overall O(n^2) where n is amount of blocks
+        for cur_left, cur_length in positions:
+            ceiling = 0
+
+            # check against all blocks currently present
+            # NOTE: if self-balancing BST is used then this block can be turn
+            # into the O(logn) with overall complexity turning to O(nlogn)
+            for block_idx, block_offset in enumerate(offsets):
+                block_left = positions[block_idx][0]
+                block_right = block_left + positions[block_idx][1]
+
+                if intersects((cur_left, cur_left + cur_length), (block_left, block_right)):
+                    ceiling = max(ceiling, block_offset + positions[block_idx][1])
+
+            offsets.append(ceiling)
+
+            rolling_max = max(rolling_max, ceiling + cur_length)
+            results.append(rolling_max)
 
         return results
 
