697fa7286b1a92e88455ba071e7e8900883c1603
Eugene Gubenkov <gubenkoved@gmail.com>
2024-06-16 21:20:43 +0200

    problem-68-text-justification.py - pass

---
+++ problem-68-text-justification.py
@@ -0,0 +1,67 @@
+from typing import List
+from math import ceil
+
+
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        lines = []
+
+        def justify(words) -> str:
+            result = ''
+            words_len = sum(len(x) for x in words)
+
+            for idx, word in enumerate(words):
+                spaces_count_left = len(words) - idx - 1
+                extra_space_left = maxWidth - words_len - len(result)
+                result += word
+                if idx != len(words) - 1:
+                    result += ' ' * ceil(extra_space_left / spaces_count_left)
+                words_len -= len(word)
+
+            # edge case with end spaces
+            if len(result) < maxWidth:
+                result += ' ' * (maxWidth - len(result))
+
+            return result
+
+        def left_justify(words) -> str:
+            result = ' '.join(words)
+            return result + ' ' * (maxWidth - len(result))
+
+        def min_len(line):
+            return sum(len(x) for x in line) + len(line) - 1
+
+        line = []
+        for word in words:
+            if min_len(line + [word]) <= maxWidth:
+                line.append(word)
+            else:
+                lines.append(line)
+                line = [word]
+
+        if line:
+            lines.append(line)
+
+        result = []
+        for idx, line in enumerate(lines):
+            if idx < len(lines) - 1:
+                result.append(justify(line))
+            else:
+                result.append(left_justify(line))
+
+        return result
+
+
+if __name__ == '__main__':
+    x = Solution()
+
+    def case(words, maxWidth):
+        result = x.fullJustify(words, maxWidth)
+        for line in result:
+            print('"%s"' % line)
+        print('***')
+
+    case(["This", "is", "an", "example", "of", "text", "justification."], maxWidth=16)
+    case(["What","must","be","acknowledgment","shall","be"], maxWidth = 16)
+    case(["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], maxWidth=20)
+    case(["Listen","to","many,","speak","to","a","few."], 6)
