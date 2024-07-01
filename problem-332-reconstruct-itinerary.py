8bc0fe5f0fafa930737a867c31cf88ec43b2435c
Eugene Gubenkov <gubenkoved@gmail.com>
2024-07-01 19:44:33 +0200

    problem-332-reconstruct-itinerary.py - hm... it works...

--- problem-332-reconstruct-itinerary.py
+++ problem-332-reconstruct-itinerary.py
@@ -16,36 +16,58 @@ class Solution:
 
         def find(cur, path, available):
             if len(path) == len(tickets) + 1:
-                results.append(tuple(path))
+                return path
 
-            for next_ticket in list(available[cur]):
+            for next_ticket in sorted(available[cur]):
                 if available[cur][next_ticket] == 0:
                     continue
                 available[cur][next_ticket] -= 1
                 next_dest = next_ticket[1]
                 path.append(next_dest)
-                find(next_dest, path, available)
+                sub_result = find(next_dest, path, available)
+                if sub_result is not None:
+                    return sub_result
                 path.pop(-1)
                 available[cur][next_ticket] += 1
 
-        # for start in all_possible:
-        #     find(start, [start], tickets_by_source)
+        return find('JFK', ['JFK'], tickets_by_source)
 
-        find('JFK', ['JFK'], tickets_by_source)
 
-        return list(min(results))
+case_idx = 0
+
+
+def visualize(tickets):
+    import pyvis
+    global case_idx
+    case_idx += 1
+    g = pyvis.network.Network(directed=True, height='1000px', width='100%')
+    nodes = set()
+    for source, dest in tickets:
+        if source not in nodes:
+            g.add_node(source)
+            nodes.add(source)
+        if dest not in nodes:
+            g.add_node(dest)
+            nodes.add(dest)
+        g.add_edge(source, dest)
+    # g.barnes_hut()
+    g.force_atlas_2based()
+    g.toggle_physics(False)
+    g.write_html('problem-332-case-%s.html' % case_idx, open_browser=False)
 
 
 if __name__ == '__main__':
     x = Solution()
-    # print(x.findItinerary(
-    #     [["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]]))
-    # print(x.findItinerary(
-    #     [["EZE", "AXA"], ["TIA", "ANU"], ["ANU", "JFK"], ["JFK", "ANU"], ["ANU", "EZE"], ["TIA", "ANU"], ["AXA", "TIA"],
-    #      ["TIA", "JFK"], ["ANU", "TIA"], ["JFK", "TIA"]]
-    # ))
-    print(x.findItinerary(
-        [["AXA", "EZE"], ["EZE", "AUA"], ["ADL", "JFK"], ["ADL", "TIA"], ["AUA", "AXA"], ["EZE", "TIA"], ["EZE", "TIA"],
+
+    def case(tickets):
+        visualize(tickets)
+        print(x.findItinerary(tickets))
+
+    case([["MUC", "LHR"], ["JFK", "MUC"], ["SFO", "SJC"], ["LHR", "SFO"]])
+    case([["EZE", "AXA"], ["TIA", "ANU"], ["ANU", "JFK"], ["JFK", "ANU"], ["ANU", "EZE"], ["TIA", "ANU"], ["AXA", "TIA"],
+          ["TIA", "JFK"], ["ANU", "TIA"], ["JFK", "TIA"]])
+    # limits exceeded
+    case([["AXA", "EZE"], ["EZE", "AUA"], ["ADL", "JFK"], ["ADL", "TIA"], ["AUA", "AXA"], ["EZE", "TIA"], ["EZE", "TIA"],
          ["AXA", "EZE"], ["EZE", "ADL"], ["ANU", "EZE"], ["TIA", "EZE"], ["JFK", "ADL"], ["AUA", "JFK"], ["JFK", "EZE"],
          ["EZE", "ANU"], ["ADL", "AUA"], ["ANU", "AXA"], ["AXA", "ADL"], ["AUA", "JFK"], ["EZE", "ADL"], ["ANU", "TIA"],
          ["AUA", "JFK"], ["TIA", "JFK"], ["EZE", "AUA"], ["AXA", "EZE"], ["AUA", "ANU"], ["ADL", "AXA"], ["EZE", "ADL"],
@@ -59,4 +81,4 @@ if __name__ == '__main__':
          ["EZE", "JFK"], ["AUA", "ADL"], ["ADL", "AUA"], ["EZE", "ANU"], ["ADL", "ANU"], ["AUA", "AXA"], ["AXA", "TIA"],
          ["AXA", "TIA"], ["ADL", "AXA"], ["EZE", "AXA"], ["AXA", "JFK"], ["JFK", "AUA"], ["ANU", "ADL"], ["AXA", "TIA"],
          ["ANU", "AUA"], ["JFK", "EZE"], ["AXA", "ADL"], ["TIA", "EZE"], ["JFK", "AXA"], ["AXA", "ADL"], ["EZE", "AUA"],
-         ["AXA", "ANU"], ["ADL", "EZE"], ["AUA", "EZE"]]))
+         ["AXA", "ANU"], ["ADL", "EZE"], ["AUA", "EZE"]])
