ef525fbe108c1b253940b65523714ce99dbb8b73
Eugene Gubenkov <gubenkoved@gmail.com>
2025-05-31 15:32:30 +0200

    problem-2942-find-words-containing-character.py - pass

---
+++ problem-2942-find-words-containing-character.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        return [idx for idx, word in enumerate(words) if x in word]
