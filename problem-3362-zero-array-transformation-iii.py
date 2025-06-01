49b4f7298f3f217e2fb2190b7e5bdb28d1f3a48f
Eugene Gubenkov <gubenkoved@gmail.com>
2025-06-01 15:34:04 +0200

    problem-3362-zero-array-transformation-iii.py - a bit simpler

--- problem-3362-zero-array-transformation-iii.py
+++ problem-3362-zero-array-transformation-iii.py
@@ -22,8 +22,8 @@ class Solution:
         # end point processed first, so we go with (-end, idx)
         available = []
 
-        picked = set()
-        cur_query_usage = 0
+        used = set()
+        usage_counter = 0
 
         for cell_idx in range(n):
             # update the heap with "available" queries
@@ -31,7 +31,7 @@ class Solution:
                 heapq.heappush(available, (-queries[query_by_start_idx][1], query_by_start_idx))
                 query_by_start_idx += 1
 
-            while nums[cell_idx] > cur_query_usage:
+            while nums[cell_idx] > len(used):
                 if not available:
                     return -1
                 # greedily pick a new "query"
@@ -41,22 +41,21 @@ class Solution:
                     # out of range already, skip
                     continue
 
-                cur_query_usage += 1
-                picked.add(picked_query_idx)
+                used.add(picked_query_idx)
+                usage_counter += 1
 
             # evict queries which go out of range
             while query_by_end_idx < len(queries) and queries_end_sorted[query_by_end_idx][0] == cell_idx:
                 query_idx = queries_end_sorted[query_by_end_idx][1]
-                if query_idx in picked:
-                    cur_query_usage -= 1
+                used.discard(query_idx)
                 query_by_end_idx += 1
 
-        return len(queries) - len(picked)
+        return len(queries) - usage_counter
 
 
 if __name__ == '__main__':
     x = Solution()
-    # print(x.maxRemoval(nums=[2, 0, 2], queries=[[0, 2], [0, 2], [1, 1]]))
-    # print(x.maxRemoval(nums=[1, 1, 1, 1], queries=[[1, 3], [0, 2], [1, 3], [1, 2]]))
-    # print(x.maxRemoval(nums=[1, 2, 3, 4], queries=[[0, 3]]))
-    print(x.maxRemoval([0,0,3], [[0,2],[1,1],[0,0],[0,0]]))
+    print(x.maxRemoval(nums=[2, 0, 2], queries=[[0, 2], [0, 2], [1, 1]]))
+    print(x.maxRemoval(nums=[1, 1, 1, 1], queries=[[1, 3], [0, 2], [1, 3], [1, 2]]))
+    print(x.maxRemoval(nums=[1, 2, 3, 4], queries=[[0, 3]]))
+    print(x.maxRemoval([0, 0, 3], [[0, 2], [1, 1], [0, 0], [0, 0]]))
