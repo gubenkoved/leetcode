2a726182c76ad41c94d6f7ce7e1801f32be6b062
Eugene Gubenkov <gubenkoved@gmail.com>
2025-06-05 19:56:31 +0200

    problem-1295-find-numbers-with-even-number-of-digits.py - faster

--- problem-1295-find-numbers-with-even-number-of-digits.py
+++ problem-1295-find-numbers-with-even-number-of-digits.py
@@ -1,5 +1,11 @@
 from typing import List
+import math
 
 class Solution:
     def findNumbers(self, nums: List[int]) -> int:
-        return sum(1 for x in nums if len(str(x)) % 2 == 0)
+        return sum(1 for x in nums if math.floor(math.log10(x)) % 2 == 1)
+
+if __name__ == '__main__':
+    x = Solution()
+    print(x.findNumbers([100000]))
+    print(x.findNumbers([100000 - 1]))
