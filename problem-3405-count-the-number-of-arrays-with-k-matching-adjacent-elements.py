eec3e774b23cec1a78c1835c09e3d672ec630bc0
Eugene Gubenkov <gubenkoved@gmail.com>
2025-06-17 21:01:50 +0200

    problem-3405-count-the-number-of-arrays-with-k-matching-adjacent-elements.py - faster

--- problem-3405-count-the-number-of-arrays-with-k-matching-adjacent-elements.py
+++ problem-3405-count-the-number-of-arrays-with-k-matching-adjacent-elements.py
@@ -3,7 +3,13 @@ import math
 class Solution:
     def countGoodArrays(self, n: int, m: int, k: int) -> int:
         mod = 10 ** 9 + 7
-        result = m * ((m - 1) ** (n - k - 1)) * math.comb(n - 1, k)
+        result = m
+
+        for x in range(n - k - 1):
+            result *= m - 1
+            result %= mod
+
+        result *= math.comb(n - 1, k) % mod
         return result % mod
 
 
@@ -11,3 +17,4 @@ if __name__ == '__main__':
     x = Solution()
     print(x.countGoodArrays(3, 2, 1))
     print(x.countGoodArrays(4, 2, 2))
+    print(x.countGoodArrays(10000, 10000, 5000))
