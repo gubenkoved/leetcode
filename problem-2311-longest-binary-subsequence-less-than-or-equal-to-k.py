047827b535536379157b215b08067b66f0d57405
Eugene Gubenkov <gubenkoved@gmail.com>
2025-06-26 21:25:47 +0200

    problem-2311-longest-binary-subsequence-less-than-or-equal-to-k.py - simpler

--- problem-2311-longest-binary-subsequence-less-than-or-equal-to-k.py
+++ problem-2311-longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -2,8 +2,20 @@ class Solution:
     def longestSubsequence(self, s: str, k: int) -> int:
         # start from the right side and extend the resulting sequence while
         # it still less than k greedily
-        idx = len(s) - 1
-        while idx > 0 and int(s[idx - 1:], 2) <= k:
+        idx = len(s)
+        r_num = 0
+        bit_idx = 0
+
+        while idx > 0:
+            # see if we can move idx to the left w/o crossing the threshold
+            b = 1 if s[idx - 1] == '1' else 0
+            r_num_next = r_num + (b << bit_idx)
+
+            if r_num_next > k:
+                break
+
+            r_num = r_num_next
+            bit_idx += 1
             idx -= 1
 
         # and then add all the zeros from the left side
@@ -21,3 +33,4 @@ if __name__ == '__main__':
     x = Solution()
     print(x.longestSubsequence("1001010", k = 5))
     print(x.longestSubsequence("00101001", k = 1))
+    print(x.longestSubsequence("1001111101", k = 999999999))
