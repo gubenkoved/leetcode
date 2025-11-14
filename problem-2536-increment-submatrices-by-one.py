95051bf4e5610dbfe27637805964b3fbfde19371
Eugene Gubenkov <gubenkoved@gmail.com>
2025-11-14 21:47:58 +0100

    problem-2536-increment-submatrices-by-one.py - reuse single array

--- problem-2536-increment-submatrices-by-one.py
+++ problem-2536-increment-submatrices-by-one.py
@@ -4,26 +4,20 @@ from typing import List
 class Solution:
     # query[i] = [row1i, col1i, row2i, col2i]
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-        # delta[r][c] will drive the delta for the prefix sum as scanned
-        # from left to right
-        deltas = [[0] * n for _ in range(n)]
+        A = [[0] * n for _ in range(n)]
 
         for r1, c1, r2, c2 in queries:
             for r in range(r1, r2 + 1):
-                deltas[r][c1] += 1
+                A[r][c1] += 1
                 if c2 + 1 < n:
-                    deltas[r][c2 + 1] -= 1
-
-        prefix_sums = [[0] * n for _ in range(n)]
+                    A[r][c2 + 1] -= 1
 
         for r in range(n):
-            val = 0
             for c in range(n):
-                val += deltas[r][c]
-                prefix_sums[r][c] = val
+                if c > 0:
+                    A[r][c] += A[r][c - 1]
 
-        # now we can compute the result
-        return prefix_sums
+        return A
 
 
 if __name__ == '__main__':
