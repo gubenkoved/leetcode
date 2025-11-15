96c929bb8e0c83c72a73834da797193abe8673ef
Eugene Gubenkov <gubenkoved@gmail.com>
2025-11-15 13:34:20 +0100

    problem-3234-count-the-number-of-substrings-with-dominant-ones.py - TLE 873/881

--- problem-3234-count-the-number-of-substrings-with-dominant-ones.py
+++ problem-3234-count-the-number-of-substrings-with-dominant-ones.py
@@ -16,18 +16,52 @@ class Solution:
         # if amount of zeros bigger than sqrt(n), then range can not be dominant
         # anymore as this amount squared would be bigger than n itself, so for
         # each starting index only attempt while amount of zeros is less than or
-        # equal than that
+        # equal than that;
+
+        # another optimization -- when I have given amount of ones and zeros
+        # at a given step we can actually skip some of the steps if we assume
+        # that ALL of the next K values would be zero, and we still be dominant;
+        # for instance, in subrange there are 10 ones, and 0 zeros, we can
+        # step at least next 3 and be sure ALL of them are dominant, because
+        # if all were zeros, we would then have 10 ones, 3 zeros, still dominant
+        # (may be this logic is better applies to count non dominant, as we can
+        # skip much more!)
 
         # ranges are inclusive
         for i in range(n):
-            for j in range(i, n):
+            j = i
+            while j < n:
                 ones = ones_count[j]
+
                 if i > 0:
                     ones -= ones_count[i - 1]
+
                 zeros = j - i + 1 - ones
+
                 if ones >= zeros * zeros:
-                    result += 1
+                    # how much can we step?
+                    #
+                    # ones >= (zeros + K) ^ 2
+                    # sqrt(ones) >= zeros + K
+                    # K <= sqrt(ones) - zeros
+                    K = math.floor(math.sqrt(ones) - zeros)
+
+                    # make sure we do not go out of range
+                    K = min(K, n - j - 1)
+
+                    j += K + 1
+                    result += K + 1
+                else:
+                    j += 1
+
+                # no longer possible to have dominant
                 if zeros > sqrt_n:
                     break
 
         return result
+
+
+if __name__ == '__main__':
+    x = Solution()
+    print(x.numberOfSubstrings('111'))
+    print(x.numberOfSubstrings('11111'))
