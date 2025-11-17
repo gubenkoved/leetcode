0af6c81efedca1d4a7e4b7acd109b17efebaf2a9
Eugene Gubenkov <gubenkoved@gmail.com>
2025-11-17 21:17:02 +0100

    problem-3147-taking-maximum-energy-from-the-mystic-dungeon.py - a bit faster

--- problem-3147-taking-maximum-energy-from-the-mystic-dungeon.py
+++ problem-3147-taking-maximum-energy-from-the-mystic-dungeon.py
@@ -6,18 +6,16 @@ class Solution:
         # each K basically define a separate sub-lanes which can be separately
         # processed in linear time -- this can done via calculating max of the
         # prefix sum inside each lane
-
+        n = len(energy)
         best = float('-inf')
         for lane_idx in range(k):
-            lane = energy[lane_idx::k]
-
-            # now process the lane using prefix sums, or rather suffix sums
             cur_best = float('-inf')
+            idx = n - lane_idx - 1
             cum_sum = 0
-            for val in reversed(lane):
+            while idx >= 0:
+                val = energy[idx]
                 cum_sum += val
                 cur_best = max(cur_best, cum_sum)
-
+                idx -= k
             best = max(best, cur_best)
-
         return best
