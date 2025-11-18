183aed2f75e8052fe47a97cc994dfd4136f9e7b3
Eugene Gubenkov <gubenkoved@gmail.com>
2025-11-18 19:29:12 +0100

    problem-1488-avoid-flood-in-the-city.py - pass

--- problem-1488-avoid-flood-in-the-city.py
+++ problem-1488-avoid-flood-in-the-city.py
@@ -1,31 +1,62 @@
 from typing import List
-
+from sortedcontainers import SortedList
+import bisect
 
 class Solution:
     def avoidFlood(self, rains: List[int]) -> List[int]:
         n = len(rains)
-        ans = []
-        full = set()
-        for idx in range(n):
-            if rains[idx] == 0:
-                cursor = idx + 1
-                while cursor < n:
-                    if rains[cursor] != 0 and rains[cursor] in full:
-                        break
-                    cursor += 1
-                if cursor == n:
-                    ans.append(1)  # whatever
-                else:
-                    ans.append(rains[cursor])
-                    full.discard(rains[cursor])
+        ans = [0] * n
+
+        filled_at = {}  # lake -> day it was filled
+        dry_days = SortedList()
+
+        for day in range(n):
+            if rains[day] == 0:
+                dry_days.add(day)
             else:
-                ans.append(-1)
-                if rains[idx] in full:
-                    return []
-                full.add(rains[idx])
+                ans[day] = -1
+                lake = rains[day]
+
+                if lake not in filled_at:
+                    filled_at[lake] = day
+                else:
+                    # okay, if we got there it means there would be overflow
+                    # if we did not dry it, so we have to dry it, and we should
+                    # greedily pick the EARLIEST day it could be dried
+
+                    if not dry_days:
+                        # no dry days left -> no possible
+                        return []
+
+                    lake_filled_at = filled_at[lake]
+
+                    # find the first suitable dry day
+                    dry_days_idx = bisect.bisect_left(dry_days, lake_filled_at)
+
+                    if dry_days_idx == len(dry_days):
+                        return []
+                    else:
+                        # found a dry day, update answer and drop it
+                        dry_day = dry_days[dry_days_idx]
+                        ans[dry_day] = lake
+                        dry_days.discard(dry_day)
+                        filled_at[lake] = day
+
+        # replace unused dry days with any lake number
+        while dry_days:
+            day = dry_days.pop()
+            ans[day] = 1
+
         return ans
 
 
 if __name__ == '__main__':
     x = Solution()
-    print(x.avoidFlood([1,2,0,0,2,1]))
+    print(x.avoidFlood([1, 2, 0, 0, 2, 1]))
+    print(x.avoidFlood([1, 2, 0, 0, 3, 2, 1]))
+    print(x.avoidFlood([1, 2, 0, 0, 3, 2, 1, 0, 2]))
+    print(x.avoidFlood([1, 2, 3, 0, 4, 5, 0, 0, 4, 5, 0, 1, 7]))
+    print(x.avoidFlood([1, 2, 3, 4, 5, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5]))
+    print(x.avoidFlood([1,0,2,3,0,1,2]), [-1,1,-1,-1,2,-1,-1])
+    print(x.avoidFlood([1, 2, 0, 1, 2]), [])
+    print(x.avoidFlood([1,0,2,0,3,0,2,0,0,0,1,2,3]), [-1,1,-1,2,-1,3,-1,2,1,1,-1,-1,-1])
