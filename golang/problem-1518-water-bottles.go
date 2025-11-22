fcd8c8d1e522165c77b7ecbb542d34ff0a7ac3f0
Eugene Gubenkov <gubenkoved@gmail.com>
2025-11-22 22:33:35 +0100

    problem-1518-water-bottles.go - PASS

---
+++ golang/problem-1518-water-bottles.go
@@ -0,0 +1,20 @@
+func numWaterBottles(numBottles int, numExchange int) int {
+	result := 0
+	full := numBottles
+	empty := 0
+	for {
+		result += full
+		empty += full
+		full = 0
+
+		if empty < numExchange {
+			break
+		}
+
+		// exchange
+		full = empty / numExchange
+		empty = empty % numExchange
+	}
+
+	return result
+}
