6898602251656f257593d62883cc540cd6c9b1a5
Eugene Gubenkov <gubenkoved@gmail.com>
2025-11-24 20:57:03 +0100

    problem-1018-binary-prefix-divisible-by-5.go - PASS

---
+++ golang/problem-1018-binary-prefix-divisible-by-5.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func prefixesDivBy5(nums []int) []bool {
+	x := 0
+	result := []bool{}
+	for _, b := range nums {
+		x = (x << 1) | b
+
+		var cur bool
+
+		if x%5 == 0 {
+			cur = true
+		} else {
+			cur = false
+		}
+
+		// keep x low
+		x = x % 10
+
+		result = append(result, cur)
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(prefixesDivBy5([]int{0, 1, 1}))
+}
