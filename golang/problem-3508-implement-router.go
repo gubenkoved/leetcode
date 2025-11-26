5e5ce9568d61e0350e18b7af05797b161377e931
Eugene Gubenkov <gubenkoved@gmail.com>
2025-11-26 22:50:41 +0100

    problem-3508-implement-router.go - 745/753 WA added per dest buffers

--- golang/problem-3508-implement-router.go
+++ golang/problem-3508-implement-router.go
@@ -23,8 +23,17 @@ func CreateCircularBuffer[T any](size int, isFixedSize bool) *CircularBuffer[T] 
 	}
 }
 
-func (cb *CircularBuffer[T]) AddElement(element T) {
+// return overwritten element if we evicted, and is_evicted flag
+func (cb *CircularBuffer[T]) AddElement(element T) (T, bool) {
 	if cb.isFixedSize {
+		var evicted T
+		is_evicted := false
+
+		if cb.producerIdx-cb.consumerIdx == len(cb.buffer) {
+			evicted = cb.buffer[cb.consumerIdx%len(cb.buffer)]
+			is_evicted = true
+		}
+
 		cb.buffer[cb.producerIdx%len(cb.buffer)] = element
 		cb.producerIdx += 1
 
@@ -32,8 +41,22 @@ func (cb *CircularBuffer[T]) AddElement(element T) {
 		if cb.producerIdx-cb.consumerIdx > len(cb.buffer) {
 			cb.consumerIdx += 1
 		}
+
+		return evicted, is_evicted
 	} else {
-		// TODO: grow the buffer if needed
+		if cb.producerIdx-cb.consumerIdx == len(cb.buffer) {
+			// need to grow buffer
+			new_buffer := make([]T, len(cb.buffer)*2)
+			copy(new_buffer, cb.buffer)
+			cb.buffer = new_buffer
+		}
+
+		cb.buffer[cb.producerIdx%len(cb.buffer)] = element
+		cb.producerIdx += 1
+
+		// we never overflow for this type of buffer
+		var ret T
+		return ret, false
 	}
 }
 
@@ -74,14 +97,16 @@ type Packet struct {
 }
 
 type Router struct {
-	buffer   *CircularBuffer[Packet]
-	detector map[Packet]bool
+	buffer                *CircularBuffer[Packet]
+	perDestinationBuffers map[int]*CircularBuffer[Packet]
+	detector              map[Packet]bool
 }
 
 func Constructor(memoryLimit int) Router {
 	return Router{
-		buffer:   CreateCircularBuffer[Packet](memoryLimit, true),
-		detector: make(map[Packet]bool),
+		buffer:                CreateCircularBuffer[Packet](memoryLimit, true),
+		perDestinationBuffers: make(map[int]*CircularBuffer[Packet]),
+		detector:              make(map[Packet]bool),
 	}
 }
 
@@ -98,7 +123,19 @@ func (this *Router) AddPacket(source int, destination int, timestamp int) bool {
 
 	// add a new packet
 	this.detector[packet] = true
-	this.buffer.AddElement(packet)
+	evicted, is_evicted := this.buffer.AddElement(packet)
+
+	// per destination buffers are dynamic in order to NOT preallocate
+	// a lot of memory
+	if this.perDestinationBuffers[destination] == nil {
+		this.perDestinationBuffers[destination] = CreateCircularBuffer[Packet](16, false)
+	}
+
+	this.perDestinationBuffers[destination].AddElement(packet)
+
+	if is_evicted {
+		this.perDestinationBuffers[evicted.destination].PopElement()
+	}
 
 	return true
 }
@@ -113,6 +150,9 @@ func (this *Router) ForwardPacket() []int {
 	// drop from detector
 	delete(this.detector, packet)
 
+	// update per destination buffer as well
+	this.perDestinationBuffers[packet.destination].PopElement()
+
 	return []int{
 		packet.source,
 		packet.destination,
@@ -120,29 +160,26 @@ func (this *Router) ForwardPacket() []int {
 	}
 }
 
-// TODO: how to calculate this per destination efficiently? do we need per destination buffer?
 func (this *Router) GetCount(destination int, startTime int, endTime int) int {
 	// binary search over the circular buffer
 	// note: left index we search as first element with timestamp >= startTime
 	// but last indexes we need the first index which timestamp >= endTime + 1
 	// so that we calculate all the occurrences of the endTime
-	start_idx := this.buffer.SearchIndexOf(func(packet Packet) bool {
+
+	buffer := this.perDestinationBuffers[destination]
+
+	if buffer == nil {
+		return 0
+	}
+
+	start_idx := buffer.SearchIndexOf(func(packet Packet) bool {
 		return packet.timestamp >= startTime
 	})
-	end_idx := this.buffer.SearchIndexOf(func(packet Packet) bool {
+	end_idx := buffer.SearchIndexOf(func(packet Packet) bool {
 		return packet.timestamp >= endTime+1
 	})
 
-	// filter per destination now?
-	count := 0
-	for idx := start_idx; idx < end_idx; idx++ {
-		packet := this.buffer.At(idx)
-		if packet.destination == destination {
-			count += 1
-		}
-	}
-
-	return count
+	return end_idx - start_idx
 }
 
 /**
