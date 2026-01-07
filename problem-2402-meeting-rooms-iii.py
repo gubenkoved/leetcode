93532ea2ae3f7e8d1bb7acf28e965bc285ecbe1b
Eugene Gubenkov <gubenkoved@gmail.com>
2026-01-07 18:29:55 +0100

    problem-2402-meeting-rooms-iii.py - PASS

--- problem-2402-meeting-rooms-iii.py
+++ problem-2402-meeting-rooms-iii.py
@@ -3,14 +3,16 @@ from typing import List
 import heapq
 
 class Solution:
-    # FIXME: this is not correct as we pick room which is freed first
-    #  when there are multiple rooms available
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        # heap of (freed_at, idx)
         heap = []
 
-        # add all as free at start
+        # heap of free indexes at given moment
+        free_indexes_heap = []
+
+        # add all rooms as free at the start
         for idx in range(n):
-            heapq.heappush(heap, (0, idx))
+            heapq.heappush(free_indexes_heap, idx)
 
         # sort by start time increasing
         meetings.sort(key=lambda x: x[0])
@@ -19,17 +21,28 @@ class Solution:
         freq = collections.defaultdict(lambda: 0)
 
         for start, end in meetings:
-            # find next free room
-            t, idx = heapq.heappop(heap)
-            freq[idx] += 1
 
-            if t > start:
-                # delay
+            # see which rooms will be freed at that time
+            while heap and heap[0][0] <= start:
+                _, idx = heapq.heappop(heap)
+
+                # mark room as free
+                heapq.heappush(free_indexes_heap, idx)
+
+            # if there is a free room -> pick one with the smallest ID
+            # and for this case there is no delay
+            if free_indexes_heap:
+                idx = heapq.heappop(free_indexes_heap)
+                heapq.heappush(heap, (end, idx))
+            else:
+                # no room available now, wait for the first free one
+                t, idx = heapq.heappop(heap)
+                assert t > start
                 delay = t - start
                 heapq.heappush(heap, (end + delay, idx))
-            else:
-                # no delay
-                heapq.heappush(heap, (end, idx))
+
+            # find next free room
+            freq[idx] += 1
 
         return max(freq.items(), key=lambda t: t[1])[0]
 
@@ -38,3 +51,4 @@ if __name__ == '__main__':
     x = Solution()
     print(x.mostBooked(n = 2, meetings = [[0,10],[1,5],[2,7],[3,4]]), 0)
     print(x.mostBooked(n = 3, meetings = [[1,20],[2,10],[3,5],[4,9],[6,8]]), 1)
+    print(x.mostBooked(n = 4, meetings=[[18,19],[3,12],[17,19],[2,13],[7,10]]), 0)
