31fea15a75cfadf8af356b98016b8ff0e4e4ffb5
Eugene Gubenkov <gubenkoved@gmail.com>
2026-01-29 09:30:02 +0100

    problem-3651-minimum-cost-path-with-teleportations.py - TLE 575/880

--- problem-3651-minimum-cost-path-with-teleportations.py
+++ problem-3651-minimum-cost-path-with-teleportations.py
@@ -34,10 +34,10 @@ class Solution:
         while heap:
             cost, k_left_neg, pos = heapq.heappop(heap)
 
-            if pos in visited:
+            if (pos, k_left_neg) in visited:
                 continue
 
-            visited.add(pos)
+            visited.add((pos, k_left_neg))
 
             if pos == finish:
                 return cost
@@ -52,24 +52,27 @@ class Solution:
             for nr, nc in candidates:
                 if nr >= rows or nc >= cols:
                     continue
-                if (nr, nc) not in visited:
-                    heapq.heappush(heap, (cost + grid[nr][nc], k_left_neg, (nr, nc)))
+                heapq.heappush(heap, (cost + grid[nr][nc], k_left_neg, (nr, nc)))
 
             # tele-neighbors
             if k_left_neg < 0:
                 for nr, nc in teleport_targets[(r,c)]:
-                    if (nr, nc) not in visited:
-                        heapq.heappush(heap, (cost, k_left_neg + 1, (nr, nc)))
+                    heapq.heappush(heap, (cost, k_left_neg + 1, (nr, nc)))
 
         assert False, 'unreachable'
 
 
 if __name__ == '__main__':
     x = Solution()
-    print(x.minCost([[6, 7, 1,  20, 11],
-                     [4, 5, 18, 23, 28]], 3), 46)
+    # print(x.minCost([[6, 7, 1,  20, 11],
+    #                  [4, 5, 18, 23, 28]], 3), 46)
+    #
+    # print(x.minCost([[9, 2, 7, 9,17,11,6, 23,24],
+    #                  [17,10,19,8,19,18,27,16,23]], 2), 23)
 
-    print(x.minCost([[9, 2, 7, 9,17,11,6, 23,24],
-                     [17,10,19,8,19,18,27,16,23]], 2), 23)
-
-    print(x.minCost([[19,10],[23,13],[16,32],[38,41],[30,36],[53,31]], 1), 55)
+    print(x.minCost([[19,10],
+                     [23,13],
+                     [16,32],
+                     [38,41],
+                     [30,36],
+                     [53,31]], 1), 55)
